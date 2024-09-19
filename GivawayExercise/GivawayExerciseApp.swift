99c085b20d4d96ff11cca0e6c3038444a0ab3c69
Spificus <64757984+Spificus@users.noreply.github.com>
2024-09-18 21:06:45 -0600

    Initial Commit

---
+++ GivawayExercise/GivawayExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GivawayExerciseApp.swift
+//  GivawayExercise
+//
+//  Created by Ian  McKell on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GivawayExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GivawayExerciseTests/GivawayExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GivawayExerciseTests.swift
+//  GivawayExerciseTests
+//
+//  Created by Ian  McKell on 9/18/24.
+//
+
+import Testing
+@testable import GivawayExercise
+
+struct GivawayExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
